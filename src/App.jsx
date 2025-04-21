11d438b8083828c57d05640aede92a63dc72d590
Asmita Gupta <asmitagupta217@gmail.com>
2025-04-21 23:31:23 +0530

    Final: Added clean chatbot and updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import Preloader from "./pages/Preloader";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
     const [loading, setLoading] = useState(true);
 
     return (
-        <>
-            {loading ? <Preloader onFinish={() => setLoading(false)} /> : <Home />}
-        </>
+        <div className="min-h-screen d-flex flex-column">
+            <Navbar />
+            <main className="flex-grow-1">
+                {loading ? <Preloader onFinish={() => setLoading(false)} /> : <Home />}
+            </main>
+            <Footer />
+        </div>
     );
 }
 
